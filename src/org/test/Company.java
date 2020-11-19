f161505fe92fc917495bb44497fd7de779735d93
sarathy2511 <sarathy2511@gmail.com>
2020-11-19 20:33:45 +0530

    ok

---
+++ src/org/test/Company.java
@@ -0,0 +1,12 @@
+package org.test;
+
+public class Company {
+	public Company() {
+		// TODO Auto-generated constructor stub
+	System.out.println("parent default");
+	
+	}
+Company(String name){
+	System.out.println("parent name :" +name);
+}
+}
---
+++ src/org/test/Employee.java
@@ -0,0 +1,17 @@
+package org.test;
+
+public class Employee extends Company {
+	public Employee() {
+		super("java");
+	System.out.println("child default");
+	// TODO Auto-generated constructor stub
+	}
+Employee(int id){
+	System.out.println("id is :"+id);
+}
+public static void main(String[] args) {
+	Employee e = new Employee();
+	Employee e1 = new Employee(444);
+	
+}
+}
